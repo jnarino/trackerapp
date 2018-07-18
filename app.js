61c0efdc90e17b6b20a55ae371ea679714f36b4d
jnarino <jnarino@summan.com>
2018-07-18 14:26:20 -0500

    first functional code

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-const http = require('http');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-//const ip = 192.168.210.59;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
- // console.log(ip); // => 8.8.8.8
-  const publicIp = require('public-ip');
-  publicIp.v4().then(ip => {
-        console.log(ip);
-        //=> '46.5.21.123'
-  var geoip = require('geoip-lite');
- // var ip = "200.24.0.20";
-  var geo = geoip.lookup(ip);
-  console.log(geo);
-
-});
-
- 
-  //var geoip = require('geoip-lite');
-  //var ip = "200.24.0.20";
-  //var geo = geoip.lookup(ip);
-  //console.log(geo);
-
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 

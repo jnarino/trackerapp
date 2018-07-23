3724327cb745dfa91a01a4b867b08f9d8c37ab3c
jnarino <jnarino@summan.com>
2018-07-22 23:36:06 -0500

    proyecto sin docker

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
+    fs.readFile('./test.html', null, function(error, data) {
         if (error) {
             response.writeHead(404);
             response.write('File not found!');

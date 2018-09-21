d8fad79c808dfcc9af6ec8d8cb956ba2637ff841
jnarino <jnarino@eafit.edu.co>
2018-09-21 10:42:29 -0500

    Update inicio.js

--- proyecto1/routes/inicio.js
+++ proyecto1/routes/inicio.js
@@ -4,7 +4,7 @@ var mongo = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://mongo-server:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
